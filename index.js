949360cdceebb91ff03152c9ef468f8454fa94ca
Matias Mariani <hi@matiasmariani.io>
2018-01-08 14:36:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ app.get('/test', function(req, res){ // listens for request on /api route
 
 app.get('/swapi', function(req, res){ // listens for request on /api route
  console.log('test api!');
+ 
+  request('https://api.hubapi.com/content/api/v2/blog-posts?hapikey=65b8decf-66bc-43bf-91db-3d862cccc152&state=published', function (error, response, body) { // api url
+ if (!error && response.statusCode === 200) {
+       console.log('received', response, body);
+      res.send(body); // if no errors, send the body of data back to front end
+    }
+  });
+
+
+app.get('/hubspot', function(req, res){ // listens for request on /api route
+ console.log('test api!');
 
  var search = req.query.search;
  console.log(search)
